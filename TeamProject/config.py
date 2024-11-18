9642be48b78e607e3ed3e07b10c885d238f930ee
Sandro Juric <JURICS@haynesboone.com>
2024-11-18 17:09:04 -0600

    modified:   TeamProject/Main.py 	modified:   TeamProject/config.py 	new file:   TeamProject/degree.py

--- TeamProject/config.py
+++ TeamProject/config.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import preprocessing
 
+
 mysql = {
     "host": "localhost",
     "user": "7330Team",
     "password": "smu-team",
     "db": "degrees",
 }
+
--- TeamProject/degree.py
+++ TeamProject/degree.py
@@ -1,22 +1,7 @@
-#!/usr/bin/env python
-import config as cfg
-import mysql.connector
-from mysql.connector import errorcode
-
 from nicegui import ui
 from nicegui.events import ValueChangeEventArguments
 
-def setup_db_connection(user, password, host, database):
-    try:
-        return mysql.connector.connect(user=user, password=password, host=host, database=database)
 
-    except mysql.connector as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not")
-        else:
-            print("Something is wrong with your user name or")
 
 def get_degrees(conn):
     cursor = conn.cursor(dictionary=True)
@@ -33,23 +18,8 @@ def get_degrees(conn):
 
     return rows
 
-
-
-db_conn = setup_db_connection(cfg.mysql["user"], cfg.mysql["password"], cfg.mysql["host"], cfg.mysql["db"])
-
-print("welcome to " + db_conn.get_server_info())
-
-
-
-columns = [
-    {'field': 'name', 'editable': True, 'sortable': True},
-    {'field': 'level', 'editable': True},
-]
-rows = get_degrees(db_conn)
-
-
 @ui.page('/')
-def page():
+def page(rows, columns):
     def add_row():
         new_id = max((dx['id'] for dx in rows), default=-1) + 1
         rows.append({'id': new_id, 'name': 'New name', 'age': None})
@@ -77,8 +47,4 @@ def page():
     ui.button('Delete selected', on_click=delete_selected)
     ui.button('New row', on_click=add_row)
 
-
 ui.run()
-
-
-db_conn.close()
