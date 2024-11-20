54abfae2ce54d71598c24f5a948c189bda19264b
Cameron Miller <cameron@millerhouse.net>
2024-11-20 17:39:28 -0600

    Set the root filepath with os and sys libraries

--- TeamProject/Main.py
+++ TeamProject/Main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from nicegui import ui
 import Menu
 from TeamProject.Pages import Course, Degree, Section, Instructor, Goal
