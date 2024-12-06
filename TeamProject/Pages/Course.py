0dd252f22f10d5d8f9d340d8506a93a7fc056af8
zeal-zachary-suzuki <165426633+zeal-zachary-suzuki@users.noreply.github.com>
2024-12-06 17:16:47 -0600

    Write out all the methods for Course.py

--- TeamProject/Pages/Course.py
+++ TeamProject/Pages/Course.py
@@ -18,6 +18,36 @@ def get_course():
 
     return rows
 
+def insert_course(args):
+    conn = MySql.create_conn()
+    cursor = conn.cursor()
+
+    stmt = """
+    INSERT INTO course
+    (
+        course_id,
+        name
+    )
+    VALUES (%s, %s)    
+    """
+
+    cursor.execute(stmt, args)
+    conn.commit()
+    conn.close()
+
+def delete_course(args):
+    conn = MySql.create_conn()
+    cursor = conn.cursor()
+
+    stmt = """
+    DELETE FROM course
+    WHERE course_id = %s
+    """
+
+    cursor.execute(stmt, args)
+    conn.commit()
+    conn.close()
+
 def page():
 
     rows = get_course()
@@ -27,18 +57,46 @@ def page():
         {'field': 'name', 'editable': True, 'sortable': True},
     ]
 
-    def add_row():
+    async def add_row():
+        with ui.dialog() as dialog, ui.card():
+            first = ui.input(label="Type Course ID")
+            second = ui.input(label="Type Course Name")
+            with ui.row():
+                ui.button('Save', on_click=lambda: dialog.submit([first.value, second.value]))
+                ui.button('Cancel', on_click=lambda: dialog.close)
+
+
+        result = await dialog
+        insert_course(result)
+
+        rows.clear()
+        rows.extend(get_course())
         aggrid.update()
 
-    def handle_cell_value_change(e):
-        new_row = e.args['data']
-        ui.notify(f'Updated row to: {e.args["data"]}')
-        rows[:] = [row | new_row if row['id'] == new_row['id'] else row for row in rows]
+    async def handle_row_select_change(e):
+        selected = [row for row in await aggrid.get_selected_rows()][0]
+
+        result = get_course([selected['course_id'], selected['name']])
+
+        rows.clear()
+        rows.extend(result)
+        rows.update()
 
     async def delete_selected():
         selected_id = [row['id'] for row in await aggrid.get_selected_rows()]
         rows[:] = [row for row in rows if row['id'] not in selected_id]
         ui.notify(f'Deleted row with ID {selected_id}')
+        aggrid.update()
+    
+    async def delete_selected():
+        selected = [row for row in await aggrid.get_selected_rows()][0]
+
+        delete_course([selected['course_id'], selected['name']])
+
+        ui.notify(f'Deleted course with ID {selected['course_id']}')
+
+        rows.clear()
+        rows.extend(get_course())
         aggrid.update()
 
     aggrid = ui.aggrid({
@@ -46,7 +104,7 @@ def page():
         'rowData': rows,
         'rowSelection': 'multiple',
         'stopEditingWhenCellsLoseFocus': True,
-    }).on('cellValueChanged', handle_cell_value_change)
+    }).on('cellValueChanged', handle_row_select_change)
 
     ui.button('Delete selected', on_click=delete_selected)
     ui.button('New row', on_click=add_row)
